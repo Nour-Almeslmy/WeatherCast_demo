574a3c99310310489a3d31df76ad1996a2d0bf6f
Nour-Almeslmy <nouralmeslmy@gmail.com>
2023-01-19 14:06:12 +0200

    weather cast init

---
+++ WeatherCast/WeatherCast/Controllers/AreasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeatherCast.Common.Entities;
+
+namespace WeatherCast.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class AreasController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Area> Get()
+        {
+            var areas = GetAreas();
+
+            return areas;
+        }
+
+        private List<Area> GetAreas()
+        {
+            return new List<Area>
+            {
+                new Area("Cairo", "Sunny"),
+                new Area("Alex", "Cloudy")
+            };
+        }
+    }
+}
---
+++ WeatherCastCommon/WeatherCast.Common.Entities/Area.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherCast.Common.Entities
+{
+    public class Area
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+
+        public Area(string name, string statues)
+        {
+            this.Name = name;
+            this.Status = statues;
+        }
+
+    }
+}
