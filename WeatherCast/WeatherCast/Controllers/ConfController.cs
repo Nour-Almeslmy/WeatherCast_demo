a46ba2938ec85c7fce1fa09b1cce5e947a20dc19
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-01-25 09:40:14 +0200

    add conf controller

---
+++ WeatherCast/WeatherCast/Controllers/ConfController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherCast.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ConfController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string Get()
+        {
+            return _configuration.GetValue<string>("Key");
+        }
+    }
+}
