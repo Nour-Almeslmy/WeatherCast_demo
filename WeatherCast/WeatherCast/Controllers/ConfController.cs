b34d269eee74e6bbfdfae75b80dc257cbef808eb
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-01-25 09:48:54 +0200

    add get values from conf env and conmap

--- WeatherCast/WeatherCast/Controllers/ConfController.cs
+++ WeatherCast/WeatherCast/Controllers/ConfController.cs
@@ -13,9 +13,35 @@ namespace WeatherCast.Controllers
         {
             _configuration = configuration;
         }
+
+        [HttpGet]
         public string Get()
         {
             return _configuration.GetValue<string>("Key");
+        }
+
+        [HttpGet]
+        [Route("env")]
+        public string GetFromEnvValues()
+        {
+            var value = _configuration.GetValue<string>("fromEnv");
+            if (String.IsNullOrEmpty(value)) 
+            {
+                return "Empty Value From Environment";
+            }
+            return value;
+        }
+
+        [HttpGet]
+        [Route("map")]
+        public string GetFromConfMap()
+        {
+            var value = _configuration.GetValue<string>("fromConfMap");
+            if (String.IsNullOrEmpty(value))
+            {
+                return "Empty Value From ConfMap";
+            }
+            return value;
         }
     }
 }
